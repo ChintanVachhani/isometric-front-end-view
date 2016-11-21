0951cf972be11bc526aaea27ad03d98190f36751
ChintanVachhani <chintan.vachhani10@gmail.com>
2016-11-20 21:06:05 -0600

    Created login and register page

---
+++ js/login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Chintan on 20-11-2016.
+ */
+//Animate Swap between login and register divs
+$(document).ready(function () {
+    $("#register-link").click(function () {
+        $(this).closest("#login-box").animate({opacity: 'hide', height: 'hide'}, 'fast');
+        $("#register-box").animate({opacity: 'show', height: 'show'}, 'slow');
+    });
+    $("#login-link").click(function () {
+        $(this).closest("#register-box").animate({opacity: 'hide', height: 'hide'}, 'fast');
+        $("#login-box").animate({opacity: 'show', height: 'show'}, 'slow');
+    });
+});
