12321bb47d5f7973014301985cba2276f91f372b
Gaurav Fogla <Gaurav Fogla>
2016-11-21 18:09:30 -0600

    implemented home page

---
+++ js/post-box.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.status').click(function () {
+        $('.arrow').css("left", 0);
+    });
+    $('.photos').click(function () {
+        $('.arrow').css("left", 146);
+    });
+});
