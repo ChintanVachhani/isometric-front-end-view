a9f81696f283565f292b35400a42b190873e6f34
ChintanVachhani <chintan.vachhani10@gmail.com>
2016-11-25 14:44:33 -0600

    Implemented Home page with Profile, New Post and Search Modals

--- js/home.js
+++ js/home.js
@@ -1,3 +1,37 @@
-$(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
+$(document).ready(function () {
+    //Pass data from new post title input in Home page to Modal
+    $("#post-create-btn").click(function () {
+        $("#new-post-modal-title").val($("#new-post-title").val());
+    });
+    //Control Proifle Modal invoking and buttons
+    $("#my-profile-link").click(function () {
+        $("#profile-modal-save-btn").hide();
+        $("#profile-modal-edit-btn").show();
+    });
+    $("#profile-view-btn").click(function () {
+        $("#profile-modal-save-btn").hide();
+        $("#profile-modal-edit-btn").show();
+    });
+    $("#profile-edit-btn").click(function () {
+        $("#profile-modal-edit-btn").hide();
+        $("#profile-modal-save-btn").show();
+        enableInputs();
+    });
+    $("#profile-modal-edit-btn").click(function () {
+        $(this).hide();
+        $("#profile-modal-save-btn").show();
+        enableInputs();
+    });
+    function enableInputs() {
+        $("#profile-modal-fullname").prop('readonly', false);
+        $("#profile-modal-email").prop('readonly', false);
+        $("#profile-modal-username").prop('readonly', false);
+        $("#profile-modal-password").prop('readonly', false);
+    }
 });
+
+
+
+
+
+
